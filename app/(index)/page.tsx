75d526ca1ce11474807913446b29c516adb9a6e8
mausooq <mauchoomausooq051@gmail.com>
2024-08-24 17:03:38 +0530

    new

---
+++ app/(index)/page.tsx
@@ -0,0 +1,242 @@
+'use client'
+
+import PreviewImage from "@/components/Preview-Image";
+import useScreenSize from "@/hooks/useScreenSize";
+import { useWindow } from "@/hooks/useWindow";
+import { Chip } from "@nextui-org/chip";
+import { Accordion, AccordionItem, Card, CardBody, CardFooter, CardHeader, Divider, Image, Link } from "@nextui-org/react";
+import NextImage from "next/image";
+import SideMenu from "./components/side-menu";
+
+export default function Home() {
+  const technologies: Record<string, string[]> = {
+    language: ["HTML", "Javascript", "TypeScript", "Python", "Go",],
+    // vue: ["Vue 2/3", "Vuex", "Vue Router", "Nuxt.js", "Vitepress"],
+    style: ["Tailwind", "Bootstrap", "CSS",],
+    react: ["React", "Nuxt.js", "Redux", "React-Query",],
+    // build: ["Vite", "Webpack",],
+    // graph: ["Three.js", "Echarts",],
+    backend: ["Node.js", "Django", "Express.js", "PHP", "MySQL", "MongoDB", "Docker",],
+    tools: ["Git",],
+    AppDev:["React Native", "Android Studio", "SupaBase",],
+    DevBoard:["Arduino", "raspberry pi", "Esp32", "STM-32"],
+    // platform: ["Cloudflare", "Vercel",]
+
+  }
+
+  const chipTheme: ("default" | "primary" | "secondary" | "success" | "warning" | "danger")[] = ["primary", "secondary", "success", "warning", "danger", "default"]
+  const chipVariants: ("solid" | "bordered" | "flat" | "faded" | "shadow")[] = ["solid", "bordered", "flat", "faded", "shadow"]
+
+  const experienceList = [
+    {
+      title: "Intern",
+      company: "BEEKODER Technologies Pvt. Ltd. ",
+      time: "October - November 2023",
+      desc: "Enhanced problem-solving skills and knowledge of data structures and algorithms in Java. Utilized Java for backend application development. Gained experience with SQL for database management."
+    },
+    {
+      title: "Intern",
+      company: "Samagra Technologies ",
+      time: "January 2024",
+      desc: "Designed and deployed blockchain networks on both Hyperledger Fabric and Ethereum. Implemented smart contracts to ensure secure and tamper-proof data management. Analyzed and compared throughput and latency metrics across blockchain networks. Created graphical comparisons showing the advantages of private blockchain."
+    },
+    {
+      title: "Freelancer Web Developer ",
+      company: "EyeqDotNet ",
+      time: "2023",
+      desc: "Developed a secure login page for EyeQdotnet’s bug bounty platform. Implemented a dynamic leaderboard section to enhance user engagement. Integrated a merchandise shop seamlessly into EyeQdotnet’s website."
+    }
+  ];  
+
+  const sideProjects = [
+    {
+      title: "AI Hub powered by Cloudflare Worker AI",
+      desc: "Developed a project using Nuxt.js and Cloudflare Worker AI, facilitating users to multiple Cloudflare Worker AI functionalities. Integrated dark/light mode and OAuth login with GitHub for enhanced user experience. Additionally, empowered users to deploy their private AI sites.",
+      preview: "",
+      github: "https://github.com/larry-xue/nuxt-chat-cloudflare",
+    },
+    {
+      title: "Larry Xue Site",
+      desc: "Built a personal website using Next.js and Next UI.",
+      preview: "https://larryxue.dev/",
+      github: "https://github.com/larry-xue/larry-xue-site"
+    },
+    {
+      title: "Weather App",
+      desc: "A simple weather app using Open-Meteo API and React.",
+      preview: "https://weather-app-react-75h.pages.dev/",
+      github: "https://github.com/larry-xue/weather-app-react"
+    },
+    {
+      title: "Folder to JSON",
+      desc: "Folder to JSON is a web application utilizing the showDirectoryPicker API to enable users to select a folder on their local machine. It recursively retrieves subdirectories to generate a JSON tree structure representing the directory hierarchy.",
+      preview: "https://folder2json.larryxue.dev/",
+      github: "https://github.com/larry-xue/folder-to-json"
+    },
+    {
+      title: "Vite Vue2 Starter Template",
+      desc: "vite-vue2-starter is a template for quickly starting Vue.js 2 projects. Built on the Vite v5 build tool",
+      github: "https://github.com/larry-xue/vite-vue2-starter"
+    }
+  ]
+
+  const { isSmallScreen, showSideMenu } = useScreenSize();
+  const _window = useWindow()
+
+  return (
+    <>
+      {showSideMenu && <SideMenu />}
+      <div className="mx-auto w-full text-center mt-16 md:flex justify-center gap-12">
+        <div className="avatar hover:scale-110 duration-300 flex justify-center items-center">
+          <Image className="rounded-tl-lg rounded-br-lg" width={200} height={200} src="/avatar.jpeg" alt="avatar" as={NextImage} />
+        </div>
+        <div>
+          <h1
+            className="hover:text-shadow-3 m-0 font-heading font-black transition-all duration-300 text-primary text-4xl md:text-5xl mt-4">
+            Abdul Mausooq</h1>
+          <div className="flex flex-col gap-4 font-extrabold text-lg md:text-left mt-4">
+            <p>FullStack Developer 💻</p>
+            <p>Embeded System 🚀</p>
+            <p>Beginner App Developer 🤯</p>
+          </div>
+        </div>
+      </div>
+      <div className="px-4 mt-2">
+        <h4 className="hover:text-shadow-3 m-0 font-heading text-lg font-black transition-all duration-300">A
+          bit about me</h4>
+        <p>Dedicated software developer with a solid background in web development, app development, and development
+        boards. Skilled in developing responsive web applications and real-time systems. Adept in various programming
+        languages and frameworks, ready to bring technical expertise and enthusiasm to a forward-thinking team</p>
+      </div>
+      <div className="px-4 mt-4 w-full">
+        <h4
+          className="hover:text-shadow-3 m-0 font-heading text-lg font-black tracking-[-0.1rem] transition-all duration-300">
+        </h4>
+        <Accordion variant="splitted" selectionMode="multiple" defaultExpandedKeys={["technologies and tools", "personal traits"]}>
+          <AccordionItem
+            title="Technologies and Tools"
+            key="technologies and tools"
+            aria-label="technologies and tools"
+          >
+            {Object.keys(technologies).map((key) => (
+              <div key={key} className="flex flex-wrap gap-2 mb-2">
+                {technologies[key].map((tech) => (
+                  <Chip
+                    key={tech}
+                    color={chipTheme[Math.floor(Math.random() * chipTheme.length)]}
+                    variant={chipVariants[Math.floor(Math.random() * chipVariants.length)]}
+                  >
+                    {tech}
+                  </Chip>
+                ))}
+              </div>
+            ))}
+          </AccordionItem>
+          <AccordionItem
+            title="Personal Traits"
+            key="personal traits"
+            aria-label="Personal Traits"
+          >
+            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400 leading-loose">
+            🎓 <span className="font-bold">Aspiring Computer Scientist</span>:Dedicated to mastering the world of technology and software development.
+            </p>
+
+            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400 leading-loose">
+            💻<span className="font-bold">Web Development Enthusiast</span>: Passionate about creating responsive and user-friendly web applications.
+            </p>
+
+            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400 leading-loose">
+              🦆<span className="font-bold">Work-Life Balance Advocate</span>: Valuing work-life balance, I prioritize maintaining harmony between professional and personal life.
+            </p>
+
+            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400 leading-loose">
+              🐸<span className="font-bold">Technical Excellence Pursuer</span>: Detail-oriented and constantly striving for excellence in technical implementations, I stay updated with the latest advancements in the field.
+            </p>
+
+            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400 leading-loose">
+              🐲<span className="font-bold">AI Technology Follower</span>: I'm particularly interested in AI technology, especially in how it can be utilized to enhance work efficiency. I enjoy experimenting with various AI tools and staying updated on the latest advancements in the field.
+            </p>
+          </AccordionItem>
+        </Accordion>
+      </div>
+
+      <div className="my-12 px-4 w-full" id="experience">
+        <h1
+          className="hover:text-shadow-3 m-0 font-heading text-[clamp(3rem,_10vw,_4.5rem)] font-black tracking-[-0.2rem] transition-all duration-300">
+          Experience</h1>
+
+        <Accordion selectionMode="multiple" defaultExpandedKeys={experienceList.map((exp) => exp.title)}>
+          {experienceList.map((exp, idx) => (
+            <AccordionItem
+              title={exp.title}
+              subtitle={exp.company + exp.time}
+              key={exp.title}
+              aria-label={exp.title}
+            >
+              <p className="mb-2 text-sm text-gray-500 dark:text-gray-400 leading-loose">
+                {exp.desc}
+              </p>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div >
+
+      <div className="mb-12 px-4 w-full" id="education">
+        <h1
+          className="hover:text-shadow-3 m-0 font-heading text-[clamp(3rem,_10vw,_4.5rem)] font-black tracking-[-0.2rem] transition-all duration-300">
+          Education</h1>
+        <div>
+          <h3 className="text-2xl font-bold">Bachelor of Computer Science Engineering</h3>
+          <p className="text-lg">P.A. College Of Engineering (2024 - 2025)</p>
+        </div>
+      </div>
+
+      <div className="mb-12 px-4" id="side-projects" >
+        <h1
+          className="hover:text-shadow-3 m-0 font-heading text-[clamp(3rem,_10vw,_4.5rem)] font-black tracking-[-0.2rem] transition-all duration-300">
+          Side Projects</h1>
+        <div className="flex flex-wrap gap-4">
+          {
+            sideProjects.map((project) => (
+              <Card className="md:w-[300px] w-full" key={project.github}>
+                <CardHeader className="flex gap-3">
+                  <h2 className="text-md">{project.title}</h2>
+                </CardHeader>
+                <Divider />
+                <CardBody>
+                {project.preview && _window?.self === _window?.top && !isSmallScreen ? (
+                    <div className="flex gap-2 h-[400px]">
+                      <p className="text-sm w-1/3 text-gray-500 dark:text-gray-400 leading-loose">{project.desc}</p>
+                      <PreviewImage url={project.preview} />
+                    </div>
+                  ) : (
+                    <p className="text-sm text-gray-500 dark:text-gray-400 leading-loose">{project.desc}</p>
+                  )}
+                </CardBody>
+                <Divider />
+                <CardFooter className="flex gap-3">
+                  {/* <Link
+                    isExternal
+                    showAnchorIcon
+                    href={project.preview}
+                    isDisabled={!project.preview}
+                  >
+                    Preview
+                  </Link> */}
+                  <Link
+                    isExternal
+                    showAnchorIcon
+                    href={project.github}
+                    isDisabled={!project.github}
+                  >
+                    GitHub
+                  </Link>
+                </CardFooter>
+              </Card>
+            ))
+          }
+        </div >
+      </div >
+    </ >
+  );
+}
---
+++ components/layout/navbar.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+import { Tabs, Tab } from "@nextui-org/react";
+import { usePathname } from "next/navigation";
+import { ThemeSwitcher } from "../theme-switcher";
+
+export default function Navbar() {
+  const pathname = usePathname();
+
+  return (
+    <div className="w-screen h-auto fixed top-4 flex justify-center items-center light-green z-50">
+      <Tabs aria-label="menu" selectedKey={pathname} defaultSelectedKey="/" color="default" radius="full">
+        <Tab key="/" href="/" title="Home" />
+        <Tab key="/blog" href="/blog" title="Blog" />
+        <Tab key="/contact" href="/contact" title="Contact" />
+        <Tab key="resume" href="https://drive.google.com/file/d/1Sz4lQpFaZgXLu7nQVd1vxtvTguW6Aroo/view?usp=sharing" target="_blank" title="Resume" />
+        <Tab key="theme" className="p-0" title={<ThemeSwitcher />} onClick={() => window.location.reload()} />
+      </Tabs>
+    </div>
+  );
+}
